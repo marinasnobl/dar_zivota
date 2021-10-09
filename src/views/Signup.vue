3520a76b30d15956bd9ec29539789616e00479ca
Marina Snobl <snoblmarina@gmail.com>
2021-10-09 13:32:29 +0200

    Izrada registracije

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -9,6 +9,7 @@
             <div class="form-group">
               <label for="exampleInputEmail1">Unesite email</label>
               <input
+                v-model="username"
                 type="email"
                 class="form-control"
                 id="exampleInputEmail1"
@@ -20,12 +21,15 @@
               <label for="exampleInputPassword1">Lozinka</label>
               <input
                 type="password"
+                v-model="password"
                 class="form-control"
                 id="exampleInputPassword1"
                 placeholder="Password"
               />
             </div>
-            <button type="submit" class="btn btn-primary">Pošalji</button>
+            <button type="button" @click="signup" class="btn btn-primary">
+              Pošalji
+            </button>
           </form>
         </div>
         <div class="col-sm"></div>
@@ -33,3 +37,32 @@
     </div>
   </div>
 </template>
+
+<script>
+import { firebaseApp } from "@/firebase.js";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+export default {
+  name: "Registracija",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    signup() {
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth,this.username, this.password)
+        .then( 
+            function() {
+                console.log('Uspjesna registracija');
+            })
+        .catch(function(error){
+            console.error('Došlo je do greške', error)
+        });
+    console.log('Nastavak');
+    },
+  },
+};
+</script>
